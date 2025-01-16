d37bf3548ef5b11f20b0643a16a64a4d084feb66
Jigar1992 <jigar.thakkar@tatvasoft.com>
2025-01-16 20:28:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyDemo // Family name
+{
+    //.NET framework : 
+    //Build >> language(C#, J#, F#, VB.NET) >> Publish >> Window(exe) and Web application (API) >>  ERP
+    //Server >> Window server
+
+
+    //.NET CORE : open source >> Platform ind...
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //debug : F10
+            // Debug >> Inside defination >> F11
+
+            // C and C++ >>> C# >> C sharp
+
+            // var abc = 10;
+
+            //string a = "JIgar";
+            //a = "Pooja";
+
+            //int b = 10; 
+
+            //int userId = 20;
+            //int userName = 20;
+
+            //char charVar = 'A';
+
+
+            //int  >> num
+            //Long >> num >> Big size 
+            //Float >> 6 to 7 Decimal
+            //Double >> 15 digit
+            //Bool >> True or true
+            //CHAR >> 'A', 'B'
+            //String >> "jigar"
+
+
+            // SImple Data types  >> Int, Flot, Char, Boolean
+
+
+            /*var ABC = 10;
+            Console.WriteLine(ABC);
+            */
+
+            // Value Types
+            // SImple Data types  >> Int, Flot, Char, Boolean
+            // Enum
+            // Struct
+            // nullable
+
+            // Reference Type >> String
+            // Class
+            // interface
+            // Array
+            // Delegates
+
+            // Type Casting  > 
+            // Implicit Type Casting (Automatic)
+            // 
+
+            //int myInt = 9;
+            //double myDouble = myInt; // There is no error >>
+
+            //// Explicit Type Casting (Manully)
+
+            //double myDoubleDemo = 10.2;
+            //int myIntDemo = (int) myDoubleDemo; // There is am error
+
+            //string temp = "Jigar";
+
+            //Console.WriteLine(myDouble);
+            //Console.WriteLine("</ br>");
+            //Console.WriteLine(myIntDemo);
+            //Console.ReadLine();
+
+
+
+            //Operators : 
+
+            // Arithmetic +,-,*,/,%
+            // (++) (--) // Increment and decrement >> 
+            // Post increment  and Pre incremetn
+            // Post D and pre D
+
+            //int a = 10;
+            //Console.WriteLine(a++); // Post increment 10
+            //Console.WriteLine(a++); // 11
+            //Console.WriteLine(a); // pre increment
+            //Console.WriteLine("<br>"); // pre increment
+            //Console.WriteLine(++a); // pre increment
+
+            //Console.ReadLine();
+
+            // Assignment
+
+            //int a = 5; // Assignment 
+            //int b = 5;
+            //// b += 10 >> b = b + 10;
+
+            //string sql = "jigar";
+            //sql += "Hello"; // sql = sql +  "Hello";
+            //sql += "Hellodsd"; // sql = sql +  "Hellodsd";
+            //sql += "Hello sds"; // sql = sql +  "Hello sds";
+            //sql += "Hello dsd s"; // sql = sql +  "Hello dsd s";
+
+
+            //Console.WriteLine(sql);
+            //Console.ReadLine();
+            // Relational  > > TRUE and False
+
+            //int a = 10;
+            //int b = 20;
+
+            //int c = 30;
+            //int d = 40;
+
+            //var result = a == b; 
+            //var result1 = a != b;
+            //var result2 = a > b; 
+            //var result3 = a < b; 
+            //var result4 = a >= b; 
+            //var result5 = a <= b; 
+
+
+
+            // Logical >> &&, ||, !
+            int a = 10;
+            int b = 20;
+
+            int c = 30;
+            int d = 40;
+
+            var result = a == b || b == c;
+
+            // 1 && 1 >> 1
+            // 1 && 0 >> 0
+            // 0 && 1 >> 0
+            // 0 && 0 >> 0
+
+            // 
+            // 1 || 1 >> 1
+            // 1 || 0 >> 1
+            // 0 || 1 >> 1
+            // 0 || 0 >> 0
+
+
+            // Bitwise 
+
+            // Unary
+            // ternary operator
+
+
+        }
+    }
+}
